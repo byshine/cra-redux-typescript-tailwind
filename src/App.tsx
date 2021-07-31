befc87896bd71a463255d138a866e4ce8113982e
Aaron Ahn <aaron@countable.ca>
2021-07-31 15:48:48 -0700

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="text-blue-400 text-4xl">
       Hello World
-      <div className="text-black text-2xl">It' like having nothing in your world yup</div>
+      <div className="text-black text-2xl">
+        It' like having nothing in your world yup
+      </div>
     </div>
   );
 }
